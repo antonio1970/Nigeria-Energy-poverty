2168f1a184a722797181b21c1caa86f789535508
ANTONIO <36565729+antonio1970@users.noreply.github.com>
2026-02-26 14:26:52 +0200

    Add files via upload

---
+++ r-script/master (2).R
@@ -0,0 +1,15 @@
+### Read dataset############################################
+#########################################################
+## Stata format coming from the do_file_cleaning.do#####
+##########################################################
+
+
+##############################################################
+## Load the necessary libraries###############################
+install.packages(c("tidyverse", "tidyr", "pacman"))
+library(pacman)
+p_load(tidyverse, tidyr)  #Using p_load to load several packages
+
+##################################################################################
+#The three composite indices: Energy security, Energy Sustainability, Energy Equity
+##################################################################################
